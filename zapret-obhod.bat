f280b083d68ca3df2634ed20f7b6f8c4d329426b
YTAJH <118638074+jester19686@users.noreply.github.com>
2024-11-10 12:45:35 +0300

    Update zapret-obhod.bat

--- zapret-obhod.bat
+++ zapret-obhod.bat
@@ -2,42 +2,65 @@
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-REM Указываем папку для хранения файлов
-set "folder=bin"
+:: Путь к текущему файлу
+set "this_file=%~f0"
 
-REM Проверка и создание папки bin, если она не существует
+:: URL к версии файла на GitHub
+set "url_self=https://raw.githubusercontent.com/jester19686/obhod/main/zapret-obhod.bat"
+
+:: Временный файл для загрузки
+set "temp_file=%temp%\zapret-obhod_temp.bat"
+
+:: Проверка, существует ли локальная версия
+echo Проверка обновлений для %this_file%...
+
+:: Загружаем текущую версию с GitHub
+powershell -Command "Invoke-WebRequest -Uri %url_self% -OutFile %temp_file%"
+
+:: Если файл успешно загружен
+if exist "%temp_file%" (
+    :: Сравниваем локальную версию с удалённой
+    fc /b "%temp_file%" "%this_file%" >nul
+    if errorlevel 1 (
+        echo Обнаружено обновление! Загружаем новую версию...
+        copy /y "%temp_file%" "%this_file%"
+        echo Программа обновлена. Перезапустите её.
+        del "%temp_file%"
+        exit /b
+    ) else (
+        echo Обновлений нет.
+    )
+) else (
+    echo Ошибка загрузки файла с GitHub.
+)
+
+:: Папка и файлы для загрузки
+set "folder=bin"
+set "flag_file=%~dp0bin\first_run_flag.txt"
+
+:: Проверка и создание папки bin
 if not exist "%folder%" (
     mkdir "%folder%"
 )
 
-REM Путь к файлу-флагу
-set "flag_file=%~dp0bin\first_run_flag.txt"
-
-REM Проверяем, существует ли файл флага
+:: Проверка первого запуска
 if not exist "%flag_file%" (
-    :: Это первый запуск, не запускаем от имени администратора
     cls
     echo Это первый запуск программы. Перезапустите программу не от имени администратора.
     echo Программа автоматически закроется через 5 секунд.
-
-    :: Создаем файл-флаг в папке bin, чтобы при последующих запусках текст не выводился
     echo This is the first run. > "%flag_file%"
-
-    :: Закрываем программу через 5 секунд
     timeout /t 5 /nobreak >nul
     exit /b
 ) else (
-    :: Это не первый запуск, проверяем, есть ли права администратора
     openfiles >nul 2>nul
     if '%errorlevel%' NEQ '0' (
-        :: Если прав нет, перезапускаем скрипт с правами администратора
         echo Необходимы права администратора. Перезапускаю скрипт с правами администратора...
         powershell -Command "Start-Process cmd -ArgumentList '/c, %~f0' -Verb RunAs"
         exit /b
     )
 )
 
-REM Указываем файлы и их URL для загрузки
+:: Указываем файлы и их URL для загрузки
 set "file1=%folder%\WinDivert.dll"
 set "url1=https://raw.githubusercontent.com/jester19686/obhod/main/bin/WinDivert.dll"
 
@@ -68,13 +91,10 @@ set "url9=https://raw.githubusercontent.com/jester19686/obhod/main/bin/winws.exe
 set "file10=COD_FIXv2.bat"
 set "url10=https://raw.githubusercontent.com/jester19686/obhod/main/COD_FIXv2.bat"
 
-REM Очищаем экран перед началом загрузки
 cls
-
-REM Выводим сообщение о начале загрузки
 echo Загрузка файлов...
 
-REM Перебираем все файлы и проверяем их наличие
+:: Проверка и загрузка файлов
 call :check_and_download "%file1%" "%url1%"
 call :check_and_download "%file2%" "%url2%"
 call :check_and_download "%file3%" "%url3%"
@@ -85,50 +105,16 @@ call :check_and_download "%file7%" "%url7%"
 call :check_and_download "%file8%" "%url8%"
 call :check_and_download "%file9%" "%url9%"
 
-REM Очищаем экран перед выводом заключительного сообщения
-cls
-
-REM После завершения всех загрузок выводим сообщение об успешном завершении
+:: Завершаем программу
 echo Все файлы успешно загружены.
-
-REM Меню выбора действия
-echo.
-
-echo.
-echo Выберите код для выполнения:
-echo 1. Временный обход
-echo 2. Постоянный обход (автозапуск)
-echo 3. Удалить обход (автозапуск)
-echo 4. Warzone (фикс-костыль)
-set /p choice=Введите номер выбранного действия (1, 2, 3, 4):
-
-if "%choice%"=="1" (
-    call :temporary_bypass
-) else if "%choice%"=="2" (
-    call :permanent_bypass
-) else if "%choice%"=="3" (
-    call :remove_bypass
-) else if "%choice%"=="4" (
-    call :warzone_fix
-) else (
-    echo Неверный выбор. Завершаю программу.
-    exit /b
-)
-
-REM Выход из программы
+pause
 exit /b
 
-pause
-goto :eof
-
 :check_and_download
-REM Эта функция проверяет наличие файла и скачивает его при необходимости
-
 set "file=%1"
 set "url=%2"
 
 if not exist "%file%" (
-    REM Только при необходимости загрузки, выводим сообщение
     echo Файл %file% не найден. Загружаю файл...
     powershell -Command "Invoke-WebRequest -Uri %url% -OutFile %file%"
     if %ERRORLEVEL% NEQ 0 (
@@ -137,7 +123,6 @@ if not exist "%file%" (
     )
 )
 
-REM Проверяем успешность загрузки
 if not exist "%file%" (
     echo Файл %file% не был загружен. Завершаю программу.
     exit /b 1
@@ -152,17 +137,16 @@ cd /d "%~dp0"
 set BIN=%~dp0bin\
 
 start "zapret: general" /min "%BIN%winws.exe" ^
-  --wf-tcp=80,443 --wf-udp=443,50000-50100 ^
-  --filter-udp=443 --hostlist="%BIN%list-general.txt" ^
-  --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" ^
-  --new --filter-udp=50000-50100 --ipset="%BIN%ipset-discord.txt" ^
-  --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 ^
-  --new --filter-tcp=80 --hostlist="%BIN%list-general.txt" ^
-  --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig ^
-  --new --filter-tcp=443 --hostlist="%BIN%list-general.txt" ^
-  --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 ^
+  --wf-tcp=80,443 --wf-udp=443,50000-50100 ^ 
+  --filter-udp=443 --hostlist="%BIN%list-general.txt" ^ 
+  --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" ^ 
+  --new --filter-udp=50000-50100 --ipset="%BIN%ipset-discord.txt" ^ 
+  --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 ^ 
+  --new --filter-tcp=80 --hostlist="%BIN%list-general.txt" ^ 
+  --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig ^ 
+  --new --filter-tcp=443 --hostlist="%BIN%list-general.txt" ^ 
+  --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 ^ 
   --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin"
-
 
 echo Временный обход выполнен.
 
@@ -172,19 +156,6 @@ exit /b
 :permanent_bypass
 REM Постоянный обход (автозапуск)
 
-REM Добавляем код для создания сервиса
-
-REM Очищаем экран перед выводом сообщения о необходимости прав администратора
-cls
-
-REM Проверяем, запущен ли скрипт от имени администратора
-openfiles >nul 2>nul
-if '%errorlevel%' NEQ '0' (
-    echo Необходимо запустить скрипт с правами администратора.
-    pause
-    exit /b
-)
-
 cls
 
 cd /d "%~dp0"
@@ -193,9 +164,6 @@ set BIN_PATH=%~dp0bin\
 
 :: Устанавливаем аргументы вручную как одну строку
 set "ARGS=--wf-tcp=80,443 --wf-udp=443,50000-50100 --filter-udp=443 --hostlist=\"%BIN_PATH%list-general.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%BIN_PATH%quic_initial_www_google_com.bin\" --new --filter-udp=50000-50100 --ipset=\"%BIN_PATH%ipset-discord.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new --filter-tcp=80 --hostlist=\"%BIN_PATH%list-general.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new --filter-tcp=443 --hostlist=\"%BIN_PATH%list-general.txt\" --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=\"%BIN_PATH%tls_clienthello_www_google_com.bin\""
-
-:: Выводим финальные аргументы для проверки
-echo Финальные аргументы: !ARGS!
 
 :: Название сервиса
 set SRVCNAME=zapret
@@ -207,100 +175,12 @@ sc delete %SRVCNAME% >nul 2>&1
 
 :: Создаём новый сервис
 echo Создание нового сервиса...
-sc create %SRVCNAME% binPath= "\"%BIN_PATH%winws.exe\" !ARGS!" DisplayName= "zapret" start= auto >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Ошибка при создании сервиса! Проверьте правильность путей и прав доступа.
-    pause
-    exit /b
-)
+sc create %SRVCNAME% binPath= "\"%BIN_PATH%winws.exe\" !ARGS!" DisplayName= "%SRVCNAME%" start= auto
 
-:: Добавляем описание к сервису
-sc description %SRVCNAME% "zapret DPI bypass software" >nul 2>&1
+:: Запуск нового сервиса
+echo Запуск нового сервиса...
+net start %SRVCNAME%
 
-:: Запускаем сервис
-echo Запуск сервиса...
-sc start %SRVCNAME% >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Ошибка при запуске сервиса! Проверьте логи и конфигурацию.
-    pause
-    exit /b
-)
+echo Сервис запущен.
 
-echo Сервис успешно создан и запущен.
-
-REM Завершаем выполнение программы
-exit /b
-
-:remove_bypass
-REM Удалить обход (автозапуск)
-
-REM Очищаем экран перед выводом сообщения о необходимости прав администратора
-cls
-
-REM Проверяем, запущен ли скрипт от имени администратора
-openfiles >nul 2>nul
-if '%errorlevel%' NEQ '0' (
-    echo Необходимо запустить скрипт с правами администратора.
-    pause
-    exit /b
-)
-
-REM После проверки прав администратора продолжаем выполнение
-
-set SRVCNAME=zapret
-
-net stop %SRVCNAME%
-sc delete %SRVCNAME%
-
-net stop "WinDivert"
-sc delete "WinDivert"
-net stop "WinDivert14"
-sc delete "WinDivert14"
-
-echo Обход (автозапуск) успешно удален.
-
-REM Завершаем выполнение программы
-exit /b
-
-
-:warzone_fix
-cls
-REM Скачиваем и проверяем наличие файла COD_FIXv2.bat, если его нет, скачиваем
-
-echo Скачиваю и проверяю наличие файла COD_FIXv2.bat...
-
-if not exist "%file10%" (
-    echo Файл %file10% не найден. Загружаю файл...
-    powershell -Command "Invoke-WebRequest -Uri %url10% -OutFile %file10%"
-    if %ERRORLEVEL% NEQ 0 (
-        echo Ошибка загрузки файла %file10%. Код ошибки: %ERRORLEVEL%
-        timeout /t 2 /nobreak >nul
-        exit /b 1
-    )
-)
-
-
-cls
-REM Проверяем успешность загрузки
-if not exist "%file10%" (
-    echo Файл %file10% не был загружен. Завершаю программу.
-    exit /b 1
-)
-
-cls
-REM Проверяем, запущен ли скрипт от имени администратора
-openfiles >nul 2>nul
-if '%errorlevel%' NEQ '0' (
-    echo Необходимо запустить скрипт с правами администратора.
-    pause
-    exit /b
-)
-
-cls
-REM Запуск файла COD_FIXv2.bat
-echo Файл COD_FIXv2.bat найден, выполняю...
-call "%file10%"
-timeout /t 2 /nobreak >nul
-
-REM Завершаем выполнение программы
 exit /b
